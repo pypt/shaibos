9d5214205c96abf89806ac9251fe1f17d382e304
Linas Valiukas <pypt@pypt.lt>
2016-04-06 12:21:46 +0300

    Don't use file which doesn't seem to be available on Python 3

--- shaibos/load/from_yaml.py
+++ shaibos/load/from_yaml.py
@@ -4,5 +4,6 @@ from shaibos.invoice import from_list_enumerate
 
 
 def load_invoices_from_yaml(yaml_path):
-    yaml_invoices = yaml_loader.load(file(yaml_path, 'r'))
+    yaml_file = open(yaml_path, 'r')
+    yaml_invoices = yaml_loader.load(yaml_file)
     return from_list_enumerate(yaml_invoices['invoices'])
