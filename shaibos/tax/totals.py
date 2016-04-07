9944631f8032bfffc0ece89d8de8f11dc3f8281d
Linas Valiukas <pypt@pypt.lt>
2016-04-07 13:29:57 +0300

    Make totals counter be aware of currency that it's counting

--- shaibos/tax/totals.py
+++ shaibos/tax/totals.py
@@ -15,6 +15,7 @@ logger = get_logger()
 class AddedTotals(UnicodeMixin):
     """Basic counter of invoice totals."""
 
+    currency = None
     income = Decimal('0.00')
     expenses = Decimal('0.00')
     tax_base = Decimal('0.00')
@@ -25,13 +26,22 @@ class AddedTotals(UnicodeMixin):
     tax = Decimal('0.00')
     profit = Decimal('0.00')
 
-    def __init__(self):
-        pass
+    def __init__(self, currency):
+        if currency is None:
+            raise Exception('Currency is undefined.')
+        self.currency = currency
 
     def __unicode__(self):
         descr_str = ""
-        descr_str += "%(income)s; expenses: %(expenses)s; tax base: %(tax_base)s; SoDra tax base: %(sodra_tax_base)s; "
-        descr_str += "VSD: %(vsd)s; PSD: %(psd)s; GPM: %(gpm)s; tax: %(tax)s; profit: %(profit)s"
+        descr_str += "%(income)s %(currency)s; "
+        descr_str += "expenses: %(expenses)s %(currency)s; "
+        descr_str += "tax base: %(tax_base)s %(currency)s; "
+        descr_str += "SoDra tax base: %(sodra_tax_base)s %(currency)s; "
+        descr_str += "VSD: %(vsd)s %(currency)s; "
+        descr_str += "PSD: %(psd)s %(currency)s; "
+        descr_str += "GPM: %(gpm)s %(currency)s; "
+        descr_str += "tax: %(tax)s %(currency)s; "
+        descr_str += "profit: %(profit)s %(currency)s"
         return descr_str % {
             'income': self.income,
             'expenses': self.expenses,
@@ -42,9 +52,13 @@ class AddedTotals(UnicodeMixin):
             'gpm': self.gpm,
             'tax': self.tax,
             'profit': self.profit,
+            'currency': self.currency,
         }
 
     def __iadd__(self, other):
+        if self.currency != other.currency:
+            raise Exception("Currencies do not match.")
+
         self.income += other.income
         self.expenses += other.expenses
         self.tax_base += other.tax_base
@@ -62,23 +76,26 @@ class CalculatedTotals(AddedTotals):
 
     countries = set()
 
-    def __init__(self, income, decimal_places, tax_rates):
-        super(CalculatedTotals, self).__init__()
+    def __init__(self, income, currency, tax_rates):
+        super(CalculatedTotals, self).__init__(currency=currency)
+
+        decimal_places = currency_decimal_places(currency=self.currency)
+
         self.income = round_to_decimal_places(Decimal(income), decimal_places)
-        self.decimal_places = decimal_places
         self.tax_rates = tax_rates
 
     def __iadd__(self, other):
-        if self.decimal_places != other.decimal_places:
-            raise TypeError("Decimal places do not match")
+        if self.currency != other.currency:
+            raise Exception("Currencies do not match.")
         if self.tax_rates != other.tax_rates:
-            raise TypeError("Tax rates do not match")
+            raise Exception("Tax rates do not match")
         self.income += other.income
         return self
 
     @property
     def expenses(self):
-        return round_to_decimal_places(self.income * self.tax_rates.expenses_rate, self.decimal_places)
+        decimal_places = currency_decimal_places(currency=self.currency)
+        return round_to_decimal_places(self.income * self.tax_rates.expenses_rate, decimal_places)
 
     @property
     def tax_base(self):
@@ -86,19 +103,23 @@ class CalculatedTotals(AddedTotals):
 
     @property
     def sodra_tax_base(self):
-        return round_to_decimal_places(self.tax_base * self.tax_rates.sodra_tax_base, self.decimal_places)
+        decimal_places = currency_decimal_places(currency=self.currency)
+        return round_to_decimal_places(self.tax_base * self.tax_rates.sodra_tax_base, decimal_places)
 
     @property
     def vsd(self):
-        return round_to_decimal_places(Decimal(self.sodra_tax_base * self.tax_rates.vsd_rate), self.decimal_places)
+        decimal_places = currency_decimal_places(currency=self.currency)
+        return round_to_decimal_places(Decimal(self.sodra_tax_base * self.tax_rates.vsd_rate), decimal_places)
 
     @property
     def psd(self):
-        return round_to_decimal_places(Decimal(self.sodra_tax_base * self.tax_rates.psd_rate), self.decimal_places)
+        decimal_places = currency_decimal_places(currency=self.currency)
+        return round_to_decimal_places(Decimal(self.sodra_tax_base * self.tax_rates.psd_rate), decimal_places)
 
     @property
     def gpm(self):
-        return round_to_decimal_places(Decimal(self.tax_base * self.tax_rates.gpm_rate), self.decimal_places)
+        decimal_places = currency_decimal_places(currency=self.currency)
+        return round_to_decimal_places(Decimal(self.tax_base * self.tax_rates.gpm_rate), decimal_places)
 
     @property
     def tax(self):
@@ -128,7 +149,7 @@ def invoice_totals(invoice, year):
     paid_amount = invoice.paid_amount()
     totals_per_invoice = CalculatedTotals(
         income=paid_amount,
-        decimal_places=currency_decimal_places(year_tax_currency),
+        currency=year_tax_currency,
         tax_rates=TaxRates.from_defaults(
             vsd_tax_percentage=invoice.seller.vsd_tax_rate,
             gpm_tax_percentage=invoice.activity.gpm_tax_rate
@@ -139,7 +160,8 @@ def invoice_totals(invoice, year):
 
 
 def buyer_totals(invoices, year):
-    totals_per_buyer = defaultdict(AddedTotals)
+    year_tax_currency = tax_currency(year)
+    totals_per_buyer = defaultdict(lambda: AddedTotals(currency=year_tax_currency))
 
     for invoice_number_prefix in invoices:
         for invoice in invoices[invoice_number_prefix]:
@@ -181,10 +203,11 @@ def activity_totals(invoices, year):
 
 
 def tax_totals(invoices, year):
+    year_tax_currency = tax_currency(year)
     totals_by_activity = activity_totals(invoices=invoices, year=year)
 
     # Add totals by activity without recalculating them because that's what VMI does
-    totals = AddedTotals()
+    totals = AddedTotals(currency=year_tax_currency)
     for t in totals_by_activity.values():
         totals += t
 
