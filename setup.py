3cede3a4b41ba123f99541550244f11f6037e785
Povilas Kanapickas <povilas@radix.lt>
2020-06-24 13:35:21 +0300

    setup: Fix support for pip v10 and newer

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 import glob
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-install_requirements = parse_requirements('requirements.txt', session=False)
-requirements = [str(ir.req) for ir in install_requirements]
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_requirements = parse_requirements('requirements.txt')
+
 
 setup(
     name='shaibos',
@@ -28,7 +34,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Office/Business :: Financial :: Accounting',
     ],
-    install_requires=requirements,
+    install_requires=install_requirements,
     test_suite='tests',
     packages=find_packages(exclude=['tests']),
     scripts=glob.glob('bin/shaibos-*'),
